24945c2b50fa86a640eee230751d871374de2a98
lucaslurf <lucas_faria@outlook.com>
2022-05-21 18:33:09 -0300

    conclusão de projeto

--- App.js
+++ App.js
@@ -1,112 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useState, useEffect} from 'react'
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import imageon from './assets/icons/eco-light.png';
+import imageoff from './assets/icons/eco-light-off.png';
+import imageDioWhite from './assets/icons/logo-dio-white.png';
+import imageDio from './assets/icons/logo-dio.png';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const App = () => {
+  const [toggle, setToggle] = useState(false); //false
+  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() =>{
+    // liga flash do celular
+    Torch.switchState(toggle);
+  }, [toggle]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  useEffect(() =>{
+    /**
+     * Quando o celular for balançado, mudaremos o toggle
+     */
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle);
+    });
+    // Essa função será chamada quando o componente
+    // For ser desmontado
+    return ()=> subscription.remove();
+  }, []);
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  return <View style= {toggle ? style.containerLight : style.container}>
+   <TouchableOpacity onPress= {handleChangeToggle}>
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+    <Image style={toggle ? style.lightingOn : style.lightingOff}
+    source={
+      toggle ? imageon : imageoff}
+      />
+        <Image style={style.dioLogo}
+    source={
+      toggle ? imageDio : imageDioWhite}
+      />
+      </TouchableOpacity>
+    </View>;
+}
 
 export default App;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems:'center',
+    justifyContent: 'center',
+  },
+
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems:'center',
+    justifyContent: 'center',
+  },
+
+  lightingOn:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+  },
+
+  lightingOff:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    tintColor: 'white',
+    width: 150,
+    height: 150,
+  },
+  
+  dioLogo:{
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 250,
+    height: 250,
+  },
+
+});
